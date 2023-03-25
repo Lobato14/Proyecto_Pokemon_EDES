4e272d3b8acffe25cb1b13d8d3a5ed5b811d4440
Rubén <rubencelislobato@gmail.com>
2023-03-25 14:44:07 +0100

    Modificacion clases Jugador y Pokemon y creacion comentarios clase Pokemon

--- src/main/kotlin/Proyecto_Pokemon/Ataque.kt
+++ src/main/kotlin/Proyecto_Pokemon/Ataque.kt
@@ -3,9 +3,13 @@ package Proyecto_Pokemon
 /**
  * ## Clase [Ataque]
  *
- * @param nombre nombre del pokemon
- * @param tipo tipo de pokemon
- * @param danio daño que recibe el pokemon
+ * Creamos una funcion [toSring] para mostrar el nombre, el tipo y el daño del pokemon.
+ *
+ * @param nombre nombre del pokemon.
+ * @param tipo tipo de pokemon.
+ * @param danio daño que recibe el pokemon. El daño no puede ser menor a 0.
+ *
+ * @author Rubén Celis Lobato y Felipe Alvarado Chamorro
  *
  */
 class Ataque(nombre:String, tipo:Tipo, danio:Int) {
@@ -20,13 +24,15 @@ class Ataque(nombre:String, tipo:Tipo, danio:Int) {
                 field = value
             }
         }
+    // Inicializamos las variables
     init {
         this.nombre = nombre
         this.tipo = tipo
         this.danio = danio
     }
+    // Función to String
     override fun toString(): String {
-        return "Ataque(nombre='$nombre', tipo=$tipo, danio=$danio)"
+        return "Pokemon(nombre='$nombre', tipo=$tipo, danio=$danio)"
     }
 
 }
