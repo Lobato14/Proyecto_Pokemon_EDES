a0d4762c89e0434e7cc8a451b89a9639686a5fe3
Rubén <rubencelislobato@gmail.com>
2023-03-25 13:12:14 +0100

    Implementacción funciones y variables en las clases Pokemon y Tipo

--- src/main/kotlin/Proyecto_Pokemon/Pokemon.kt
+++ src/main/kotlin/Proyecto_Pokemon/Pokemon.kt
@@ -29,7 +29,23 @@ class Pokemon(nombre:String, lore:String, tipo:Tipo, fuerza:Int, vida:Int) {
         this.fuerza = fuerza
         this.vida = vida
     }
+    fun recibirAtaque(ataque: Ataque) {
+        var factor = if (ataque.tipo.tipo == tipo.tipo){
+            0.5
+        } else{
+            1.0
+        }
+        var danio = (ataque.danio * factor).toInt()
+        vida -= danio
+    }
 
+    fun tipoDebil(tipoAtaque: Tipo): String {
+        return if (tipo.esDebilContra(tipoAtaque.tipo)) {
+            tipoAtaque.tipo
+        } else {
+            "Este pokemon no es débil ante el otro"
+        }
+    }
     override fun toString(): String {
         return "Pokemon(nombre='$nombre', lore='$lore', tipo='$tipo', fuerza=$fuerza, vida=$vida)"
     }
--- src/main/kotlin/Proyecto_Pokemon/Tipo.kt
+++ src/main/kotlin/Proyecto_Pokemon/Tipo.kt
@@ -7,5 +7,16 @@ class Tipo(tipo:String) {
         this.tipo = tipo
     }
 
+    var debilidades = mapOf(
+        "Agua" to listOf("Electrico"),
+        "Fuego" to listOf("Agua"),
+        "Hierba" to listOf("Fuego"),
+        "Electrico" to listOf("Tierra"),
+        "Hielo" to listOf("Fuego")
+    )
+
+    fun esDebilContra(tipoAtaque: String): Boolean {
+        return tipoAtaque in (debilidades[tipo] ?: emptyList())
+    }
 
 }
